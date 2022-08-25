60bc6670cb1e2e7a76c134e77b653ff773e8e472
jahn-moonkin21 <jmoonkin9@gmail.com>
2022-08-25 20:05:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Author: John Mentgen
+ * Date: 2022/08/25
+ * Simple hello world program for github stuff
+ */
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+    printf("hello world! This is my message");
+    return 0;
+}
